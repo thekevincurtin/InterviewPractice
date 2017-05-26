d3ec7d300d39b3464b53788a64ab80fd19b884d9
Kevin <thekevincurtin@gmail.com>
2017-05-25 21:54:18 -0400

    completed AllAnagrams

---
+++ src/AllAnagrams.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+/**
+ * Created by theke on 5/25/2017.
+ */
+public class AllAnagrams {
+    //https://leetcode.com/problems/find-all-anagrams-in-a-string
+    public List<Integer> findAnagrams(String s, String p){
+        HashMap<Character, Integer> anagram = new HashMap<>();
+        ArrayDeque<Character> deque = new ArrayDeque<>();
+        for(int i=0;i< p.length();i++){
+            if(anagram.containsKey(p.charAt(i))){
+                anagram.put(p.charAt(i), anagram.get(p.charAt(i)+1));
+            }
+            else{
+                anagram.put(p.charAt(i), 1);
+            }
+        }
+        int j = 0;
+        List<Integer> anagramIdxs = new ArrayList<>();
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        while(j<s.length()){
+            deque.add(s.charAt(j));
+            if(sMap.containsKey(s.charAt(j))){
+                sMap.put(s.charAt(j), sMap.get(s.charAt(j))+1);
+            }
+            else{
+                sMap.put(s.charAt(j), 1);
+            }
+            if(deque.size() > p.length()){
+                char pop = deque.pop();
+                if(sMap.get(pop)==1){
+                    sMap.remove(pop);
+                }
+                else{
+                    sMap.put(pop,sMap.get(pop)-1);
+                }
+            }
+
+            if(sMap.equals(anagram)){
+                anagramIdxs.add(j+1 - p.length());
+            }
+            j++;
+        }
+        return anagramIdxs;
+    }
+}
+class AnagramExec{
+    public static void main(String[] args){
+        System.out.println(new AllAnagrams().findAnagrams("hehehe", "he"));
+        System.out.println("expected 0 1 2 3 4");
+        System.out.println(new AllAnagrams().findAnagrams("cbaebabacd", "abc"));
+        System.out.println("expected 0 6");
+        System.out.println(new AllAnagrams().findAnagrams("abab", "ab"));
+        System.out.println("expected 0 1 2");
+    }
+}
---
+++ src/NumberOfIslands.java
@@ -0,0 +1,18 @@
+
+/**
+ * Created by theke on 5/25/2017.
+ */
+public class NumberOfIslands {
+    //https://leetcode.com/problems/number-of-islands
+    public int numIslands(char[][] grid){
+
+        return 0;
+    }
+}
+class Exec {
+    int result = new NumberOfIslands().numIslands(new char[][]{
+        {'0','1','1','0'},
+        {'0','0','0','0'}
+    });
+
+}
